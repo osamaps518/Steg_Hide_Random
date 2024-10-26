d09081b63cd776731fd4ef9a7eff89ddae0420cb
skass <skassis2001@gmail.com>
2024-10-26 18:38:50 +0300

    I have commited the convert bits to string method

--- src/main/java/com/stegrandom/utilites/Utils.java
+++ src/main/java/com/stegrandom/utilites/Utils.java
@@ -1,6 +1,8 @@
 package com.stegrandom.utilites;
 
 public class Utils {
+
+    //To Convert a String into a sequence of bits
     public static StringBuilder convertStringToBits(String input){
         byte[] bytes = input.getBytes();
         StringBuilder bits = new StringBuilder();
@@ -12,5 +14,21 @@ public class Utils {
             }
         }
         return bits;
+    }
+
+    public static String convertBitsToString(StringBuilder bits) {
+        StringBuilder message = new StringBuilder();
+
+        for (int i = 0; i < bits.length(); i += 8) {
+            int charCode = 0;
+
+            // Collect each byte (8 bits) and convert to a character
+            for (int j = 0; j < 8; j++) {
+                charCode = (charCode << 1) | (bits.charAt(i + j) - '0');
+            }
+            message.append((char) charCode);
+        }
+
+        return message.toString();
     }
 }
